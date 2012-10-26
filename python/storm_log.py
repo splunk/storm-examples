103512eb6b893edd3dcfdee8c2767c04b1ec619a
Greg Albrecht <gba@splunk.com>
2012-10-25 18:34:44 -0700

    Adding license boilerplate to python example, also adding additional metadata and some code cleanup.

--- python/storm_log.py
+++ python/storm_log.py
@@ -1,11 +1,36 @@
+#!/usr/bin/env python
+"""Splunk Storm Input API Python Example.
+
+Copyright 2012 Splunk, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+__author__ = 'Splunk Storm Engineering <mailto:support@splunkstorm.com>'
+__copyright__ = 'Copyright 2012 Splunk, Inc.'
+__license__ = 'Apache License 2.0'
+
+
 import urllib
 import urllib2
+
 
 # Fill the access_token and project_id values from Storm UI
 # You'll need to have an existing account with Splunk Storm
 # http://www.splunkstorm.com/
 ACCESS_TOKEN = '<your access token>'
 PROJECT_ID = '<project id>'
+
 
 class StormLog(object):
     """A simple example class to send logs to a Splunk Storm project.
